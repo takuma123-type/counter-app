5435624e82409168d20a4e293e3cf6da37f5b495
takuma123-type <seishirou1270@gmail.com>
2023-04-28 00:53:17 +0900

    表示、非表示

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,8 @@ import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
 const handleClick = useCallback((e) => {
   console.log(count)
@@ -11,6 +13,10 @@ const handleClick = useCallback((e) => {
     setCount(count => count + 1);
   }
 }, [count]);
+
+const handleDisplay = useCallback(() => {
+  setIsShow((isShow) => !isShow);
+}, [isShow]);
 
 useEffect(() => {
   console.log(`マウント時: ${count}`)
@@ -21,19 +27,28 @@ useEffect(() => {
   };
 }, []);
 
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
   return (    
     <div className={styles.container}>
       <div class="wrap">
         <Head>
           <title>Index Page</title>
         </Head>
-        <h1>{count}</h1>
-        <button 
-        href="/about"
-        onClick={handleClick}
-          >
-            ボタン
-        </button>
+        {isShow ? <h1>{count}</h1> : null}
+        <button onClick={handleClick}>ボタン</button>
+        <button onClick={handleDisplay}>
+          {isShow ? "非表示" : "表示" }</button>
+        <input 
+          type="text" 
+          value={text} 
+          onChange={handleChange} />
       </div>
     </div>
   )
